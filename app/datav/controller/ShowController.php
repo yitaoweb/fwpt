d15ee44607bcdb6d265804298859cf152fab1500
573974298@qq.com <gjy19861005>
2017-12-11 09:14:52 +0800

    sub_analysis  -lvnet

--- app/datav/controller/ShowController.php
+++ app/datav/controller/ShowController.php
@@ -85,4 +85,293 @@ class ShowController extends HomeBaseController
 
         return $this->fetch(':show');
     }
+
+    public function site()
+    {
+        //header('Access-Control-Allow-Origin:*'); 
+        //header('Access-Control-Allow-Origin:http://127.0.0.1'); 
+        header('Access-Control-Allow-Origin:*');//允许所有来源访问
+        header('Access-Control-Allow-Method:POST,GET');//允许访问的方式
+
+        header('Access-Control-Allow-Origin: *');
+
+        $site_name = cmf_get_site_info('site_info');
+        
+        $name=$site_name['site_name'];
+  
+        $title='长治市中小商贸流通企业公共服务数据分析平台';
+        $en = 'changzhi public service platform for SME';
+        $this->assign('title',$title);
+        $this->assign('en',$en);
+
+        $qy_num = Db::query('select count(id) as num from pt_user where user_type = 2');
+        $jg_num = Db::query('select count(id) as num from pt_user where user_type = 3');
+        $lxd_num = Db::query('select count(id) as num from pt_portal_lxd');
+        $fw_num = Db::query('select count(id) as num from pt_portal_fwxq');
+        $cp_num = Db::query('select count(id) as num from pt_portal_fwcp');
+        $dj_num = Db::query('select count(id) as num from pt_portal_dj');
+        $zj_num = Db::query('select count(id) as num from pt_portal_zjk');
+
+     
+        $this->assign('qy_num',$qy_num[0]['num']);
+        $this->assign('jg_num',$jg_num[0]['num']);
+        $this->assign('lxd_num',$lxd_num[0]['num']);
+        $this->assign('fw_num',$fw_num[0]['num']);
+        $this->assign('cp_num',$cp_num[0]['num']);
+        $this->assign('dj_num',$dj_num[0]['num']);
+        $this->assign('zj_num',$zj_num[0]['num']);
+
+
+        
+        $token = '224b02e8ae9c56f367c0aa71610c3d18' . '&';
+        $strs = 'GET&%2Fctr_active_anal%2Fget_offline_data&app_id%500554165%26end_date%3D2017-12-06%26idx%3D10201%2C10202%2C10203%26start_date%3D2017-11-01';
+        $sign = sign($token, $strs);
+
+        $this->assign('sign',$sign);
+        //var_dump($sign);
+        //return $name;
+
+        return $this->fetch(':site');
+    }
+
+    public function xsqy()
+    {
+        //header('Access-Control-Allow-Origin:*'); 
+        //header('Access-Control-Allow-Origin:http://127.0.0.1'); 
+        header('Access-Control-Allow-Origin:*');//允许所有来源访问
+        header('Access-Control-Allow-Method:POST,GET');//允许访问的方式
+
+        header('Access-Control-Allow-Origin: *');
+
+        $site_name = cmf_get_site_info('site_info');
+        
+        $name=$site_name['site_name'];
+  
+        $title='长治市中小商贸流通企业公共服务数据分析平台';
+        $en = 'changzhi public service platform for SME';
+        $this->assign('title',$title);
+        $this->assign('en',$en);
+
+        $qy_num = Db::query('select count(id) as num from pt_user where user_type = 2');
+        $jg_num = Db::query('select count(id) as num from pt_user where user_type = 3');
+        $lxd_num = Db::query('select count(id) as num from pt_portal_lxd');
+        $fw_num = Db::query('select count(id) as num from pt_portal_fwxq');
+        $cp_num = Db::query('select count(id) as num from pt_portal_fwcp');
+        $dj_num = Db::query('select count(id) as num from pt_portal_dj');
+        $zj_num = Db::query('select count(id) as num from pt_portal_zjk');
+
+     
+        $this->assign('qy_num',$qy_num[0]['num']);
+        $this->assign('jg_num',$jg_num[0]['num']);
+        $this->assign('lxd_num',$lxd_num[0]['num']);
+        $this->assign('fw_num',$fw_num[0]['num']);
+        $this->assign('cp_num',$cp_num[0]['num']);
+        $this->assign('dj_num',$dj_num[0]['num']);
+        $this->assign('zj_num',$zj_num[0]['num']);
+
+
+        
+        $token = '224b02e8ae9c56f367c0aa71610c3d18' . '&';
+        $strs = 'GET&%2Fctr_active_anal%2Fget_offline_data&app_id%500554165%26end_date%3D2017-12-06%26idx%3D10201%2C10202%2C10203%26start_date%3D2017-11-01';
+        $sign = sign($token, $strs);
+
+        $this->assign('sign',$sign);
+        //var_dump($sign);
+        //return $name;
+
+        return $this->fetch(':xsqy');
+    }
+
+    public function jj()
+    {
+        //header('Access-Control-Allow-Origin:*'); 
+        //header('Access-Control-Allow-Origin:http://127.0.0.1'); 
+        header('Access-Control-Allow-Origin:*');//允许所有来源访问
+        header('Access-Control-Allow-Method:POST,GET');//允许访问的方式
+
+        header('Access-Control-Allow-Origin: *');
+
+        $site_name = cmf_get_site_info('site_info');
+        
+        $name=$site_name['site_name'];
+  
+        $title='长治市中小商贸流通企业公共服务数据分析平台';
+        $en = 'changzhi public service platform for SME';
+        $this->assign('title',$title);
+        $this->assign('en',$en);
+
+        $qy_num = Db::query('select count(id) as num from pt_user where user_type = 2');
+        $jg_num = Db::query('select count(id) as num from pt_user where user_type = 3');
+        $lxd_num = Db::query('select count(id) as num from pt_portal_lxd');
+        $fw_num = Db::query('select count(id) as num from pt_portal_fwxq');
+        $cp_num = Db::query('select count(id) as num from pt_portal_fwcp');
+        $dj_num = Db::query('select count(id) as num from pt_portal_dj');
+        $zj_num = Db::query('select count(id) as num from pt_portal_zjk');
+
+     
+        $this->assign('qy_num',$qy_num[0]['num']);
+        $this->assign('jg_num',$jg_num[0]['num']);
+        $this->assign('lxd_num',$lxd_num[0]['num']);
+        $this->assign('fw_num',$fw_num[0]['num']);
+        $this->assign('cp_num',$cp_num[0]['num']);
+        $this->assign('dj_num',$dj_num[0]['num']);
+        $this->assign('zj_num',$zj_num[0]['num']);
+
+
+        
+        $token = '224b02e8ae9c56f367c0aa71610c3d18' . '&';
+        $strs = 'GET&%2Fctr_active_anal%2Fget_offline_data&app_id%500554165%26end_date%3D2017-12-06%26idx%3D10201%2C10202%2C10203%26start_date%3D2017-11-01';
+        $sign = sign($token, $strs);
+
+        $this->assign('sign',$sign);
+        //var_dump($sign);
+        //return $name;
+
+        return $this->fetch(':xm');
+    }
+
+    public function fw()
+    {
+        //header('Access-Control-Allow-Origin:*'); 
+        //header('Access-Control-Allow-Origin:http://127.0.0.1'); 
+        header('Access-Control-Allow-Origin:*');//允许所有来源访问
+        header('Access-Control-Allow-Method:POST,GET');//允许访问的方式
+
+        header('Access-Control-Allow-Origin: *');
+
+        $site_name = cmf_get_site_info('site_info');
+        
+        $name=$site_name['site_name'];
+  
+        $title='长治市中小商贸流通企业公共服务数据分析平台';
+        $en = 'changzhi public service platform for SME';
+        $this->assign('title',$title);
+        $this->assign('en',$en);
+
+        $qy_num = Db::query('select count(id) as num from pt_user where user_type = 2');
+        $jg_num = Db::query('select count(id) as num from pt_user where user_type = 3');
+        $lxd_num = Db::query('select count(id) as num from pt_portal_lxd');
+        $fw_num = Db::query('select count(id) as num from pt_portal_fwxq');
+        $cp_num = Db::query('select count(id) as num from pt_portal_fwcp');
+        $dj_num = Db::query('select count(id) as num from pt_portal_dj');
+        $zj_num = Db::query('select count(id) as num from pt_portal_zjk');
+
+     
+        $this->assign('qy_num',$qy_num[0]['num']);
+        $this->assign('jg_num',$jg_num[0]['num']);
+        $this->assign('lxd_num',$lxd_num[0]['num']);
+        $this->assign('fw_num',$fw_num[0]['num']);
+        $this->assign('cp_num',$cp_num[0]['num']);
+        $this->assign('dj_num',$dj_num[0]['num']);
+        $this->assign('zj_num',$zj_num[0]['num']);
+
+
+        
+        $token = '224b02e8ae9c56f367c0aa71610c3d18' . '&';
+        $strs = 'GET&%2Fctr_active_anal%2Fget_offline_data&app_id%500554165%26end_date%3D2017-12-06%26idx%3D10201%2C10202%2C10203%26start_date%3D2017-11-01';
+        $sign = sign($token, $strs);
+
+        $this->assign('sign',$sign);
+        //var_dump($sign);
+        //return $name;
+
+        return $this->fetch(':fw');
+    }
+
+
+
+    public function xm()
+    {
+        //header('Access-Control-Allow-Origin:*'); 
+        //header('Access-Control-Allow-Origin:http://127.0.0.1'); 
+        header('Access-Control-Allow-Origin:*');//允许所有来源访问
+        header('Access-Control-Allow-Method:POST,GET');//允许访问的方式
+
+        header('Access-Control-Allow-Origin: *');
+
+        $site_name = cmf_get_site_info('site_info');
+        
+        $name=$site_name['site_name'];
+  
+        $title='长治市中小商贸流通企业公共服务数据分析平台';
+        $en = 'changzhi public service platform for SME';
+        $this->assign('title',$title);
+        $this->assign('en',$en);
+
+        $qy_num = Db::query('select count(id) as num from pt_user where user_type = 2');
+        $jg_num = Db::query('select count(id) as num from pt_user where user_type = 3');
+        $lxd_num = Db::query('select count(id) as num from pt_portal_lxd');
+        $fw_num = Db::query('select count(id) as num from pt_portal_fwxq');
+        $cp_num = Db::query('select count(id) as num from pt_portal_fwcp');
+        $dj_num = Db::query('select count(id) as num from pt_portal_dj');
+        $zj_num = Db::query('select count(id) as num from pt_portal_zjk');
+
+     
+        $this->assign('qy_num',$qy_num[0]['num']);
+        $this->assign('jg_num',$jg_num[0]['num']);
+        $this->assign('lxd_num',$lxd_num[0]['num']);
+        $this->assign('fw_num',$fw_num[0]['num']);
+        $this->assign('cp_num',$cp_num[0]['num']);
+        $this->assign('dj_num',$dj_num[0]['num']);
+        $this->assign('zj_num',$zj_num[0]['num']);
+
+
+        
+        $token = '224b02e8ae9c56f367c0aa71610c3d18' . '&';
+        $strs = 'GET&%2Fctr_active_anal%2Fget_offline_data&app_id%500554165%26end_date%3D2017-12-06%26idx%3D10201%2C10202%2C10203%26start_date%3D2017-11-01';
+        $sign = sign($token, $strs);
+
+        $this->assign('sign',$sign);
+        //var_dump($sign);
+        //return $name;
+
+        return $this->fetch(':xm');
+    }
+    public function ent()
+    {
+        //header('Access-Control-Allow-Origin:*'); 
+        //header('Access-Control-Allow-Origin:http://127.0.0.1'); 
+        header('Access-Control-Allow-Origin:*');//允许所有来源访问
+        header('Access-Control-Allow-Method:POST,GET');//允许访问的方式
+
+        header('Access-Control-Allow-Origin: *');
+
+        $site_name = cmf_get_site_info('site_info');
+        
+        $name=$site_name['site_name'];
+  
+        $title='长治市中小商贸流通企业公共服务数据分析平台';
+        $en = 'changzhi public service platform for SME';
+        $this->assign('title',$title);
+        $this->assign('en',$en);
+
+        $qy_num = Db::query('select count(id) as num from pt_user where user_type = 2');
+        $jg_num = Db::query('select count(id) as num from pt_user where user_type = 3');
+        $lxd_num = Db::query('select count(id) as num from pt_portal_lxd');
+        $fw_num = Db::query('select count(id) as num from pt_portal_fwxq');
+        $cp_num = Db::query('select count(id) as num from pt_portal_fwcp');
+        $dj_num = Db::query('select count(id) as num from pt_portal_dj');
+        $zj_num = Db::query('select count(id) as num from pt_portal_zjk');
+
+     
+        $this->assign('qy_num',$qy_num[0]['num']);
+        $this->assign('jg_num',$jg_num[0]['num']);
+        $this->assign('lxd_num',$lxd_num[0]['num']);
+        $this->assign('fw_num',$fw_num[0]['num']);
+        $this->assign('cp_num',$cp_num[0]['num']);
+        $this->assign('dj_num',$dj_num[0]['num']);
+        $this->assign('zj_num',$zj_num[0]['num']);
+
+
+        
+        $token = '224b02e8ae9c56f367c0aa71610c3d18' . '&';
+        $strs = 'GET&%2Fctr_active_anal%2Fget_offline_data&app_id%500554165%26end_date%3D2017-12-06%26idx%3D10201%2C10202%2C10203%26start_date%3D2017-11-01';
+        $sign = sign($token, $strs);
+
+        $this->assign('sign',$sign);
+        //var_dump($sign);
+        //return $name;
+
+        return $this->fetch(':ent');
+    }
 }
